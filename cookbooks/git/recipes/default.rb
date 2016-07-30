b31c7bfa8bab78ce75fcfbac0bb5623eb15431c3
Ruslan Kenzhebekov <rkenzheb@pincsolutions.com>
2016-07-29 19:22:20 -0700

    Add known_hosts

--- cookbooks/git/recipes/default.rb
+++ cookbooks/git/recipes/default.rb
@@ -1,11 +1,12 @@
 package 'git'
 
-
 execute 'ssh_keyscan' do
-  command 'ssh-keyscan -H github.com >> ~/.ssh/known_hosts'
+  user 'vagrant'
+  command 'ssh-keyscan -H github.com >> /home/vagrant/.ssh/known_hosts'
 end
 
 execute 'ssh_T' do
   command 'ssh -T git@github.com'
   ignore_failure true
 end
+
